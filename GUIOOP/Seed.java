1326d254c58b1b746a9203a9c0e7a837b81fe3e2
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-03 13:40:52 +0700

    Delete GUIOOP directory

--- GUIOOP/Seed.java
+++
@@ -1,5 +0,0 @@
-package GUIOOP;
-
-public enum Seed {   // to save as "Seed.java"
-    CROSS, NOUGHT, NO_SEED
-}
--- GUIOOP/State.java
+++
@@ -1,5 +0,0 @@
-package GUIOOP;
-
-public enum State {  // to save as "State.java"
-    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-}
