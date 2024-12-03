3dce5cab14c73f2fbc6a0197acb46efeb8dc576f
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-03 13:39:27 +0700

    Delete Sudoku directory

--- Sudoku/SudokuConstants.java
+++
@@ -1,8 +0,0 @@
-package Sudoku;
-
-public class SudokuConstants {
-    /** Size of the board */
-    public static final int GRID_SIZE = 9;
-    /** Size of sub-grid of the board */
-    public static final int SUBGRID_SIZE = 3;
-}
